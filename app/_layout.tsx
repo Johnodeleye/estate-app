24e4717cf6f4f0e9c4a015c78a79d9704d3e813b
Perspicacious <johnayomide920@gmail.com>
2025-01-07 12:00:00 +0100

    Scheduled commit for previous days

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,17 @@
 import { Stack } from "expo-router";
-import './globals.css'
+import './globals.css';
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts(map:{
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-Extrabold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+  })
+
   return <Stack />;
 }
 
