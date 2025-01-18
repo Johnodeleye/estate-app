72bc908f973588b3e61194f7f16966c67740ae8f
Perspicacious <johnayomide920@gmail.com>
2025-01-18 11:52:29 +0100

    ho

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
@@ -6,7 +6,7 @@ import icons from '@/constants/icons'
 
 const SignIn = () => {
   const handleLogin = () => {
-    alert('hello native😁');
+    Alert.alert('Greetings Alert!💞', 'hello native😁');
   }
   return (
     <SafeAreaView className='bg-white h-full'>
@@ -22,12 +22,18 @@ const SignIn = () => {
           </Text>
 
           <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
-            Continue with Google
+            Login to Bright-Homes
           </Text>
 
           <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
-            <Image source={icons.google} className='w-5 h-5' resizeMode="contain"
+
+            <View className='flex flex-row items-center justify-center'>
+            <Image source={icons.google} className='w-5 h-5 ' resizeMode="contain"
             />
+
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+            </View>
+
           </TouchableOpacity>
 
         </View>
