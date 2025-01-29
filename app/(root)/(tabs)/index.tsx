3088c6b27770220bd4cfe2982612cdc8dcb60e4d
Perspicacious <johnayomide920@gmail.com>
2025-01-29 03:02:31 +0100

    2am commit

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,22 +2,30 @@ import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <SafeAreaView className="bg-white h-full">
+    <SafeAreaView className="h-full bg-white">
       <View className="px-5">
-        <Search/>
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
-            <Image source={images.profile} className="size-12 rounded-full border border-gray-400" />
-            <View className="flex flex-col items-start ml-2 justify-center">
+            <Image source={images.profile} className="border border-gray-400 rounded-full size-12" />
+            <View className="flex flex-col items-start justify-center ml-2">
               <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
               <Text className="text-base font-rubik-medium text-black-300">John</Text>
             </View>
           </View>
           <Image source={icons.bell} className="size-6"/>
+        </View>
+      </View>
+      <Search/>
+      <View className="px-5 my-5">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
